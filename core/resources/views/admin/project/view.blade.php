43eb724a04b2226d18df610e84f22255d4970bc6
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-08-21 21:33:57 +0600

    4545

--- core/resources/views/admin/project/view.blade.php
+++ core/resources/views/admin/project/view.blade.php
@@ -107,25 +107,6 @@
                                             </div>
                                         </div>
 
-
-
-                                        <div class="media media-chat">
-                                            <img class="avatar"
-                                                src="https://img.icons8.com/color/36/000000/administrator-male.png"
-                                                alt="...">
-                                            <div class="media-body">
-                                                <p>Sorry I don't have. i changed my phone.</p>
-                                                <p class="meta"><time datetime="2018">00:12</time></p>
-                                            </div>
-                                        </div>
-
-                                        <div class="media media-chat media-chat-reverse">
-                                            <div class="media-body">
-                                                <p>Okay then see you on sunday!!</p>
-                                                <p class="meta"><time datetime="2018">00:12</time></p>
-                                            </div>
-                                        </div>
-
                                         <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;">
                                             <div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div>
                                         </div>
